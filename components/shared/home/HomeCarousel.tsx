6b11e084647d48ffe6aed4289ea562ca7db531ea
Tassawar-Abbas <tas80066@gmail.com>
2025-02-14 10:14:29 +0500

    home carousel changes

--- components/shared/home/HomeCarousel.tsx
+++ components/shared/home/HomeCarousel.tsx
@@ -20,6 +20,7 @@ export default function HomeCarousel({ items }:
     const plugin = React.useRef(
         Autoplay({ delay: 3000, stopOnInteraction: true })
     )
+    
     return (
         <Carousel dir='ltr' plugins={[plugin.current]} className='w-full mx-auto'
             onMouseEnter={plugin.current.stop} onMouseLeave={plugin.current.reset}>
@@ -28,7 +29,8 @@ export default function HomeCarousel({ items }:
                     <CarouselItem key={item.title}>
                         <Link href={item.url}>
                             <div className="flex items-center justify-center p-6 relative -m-1 aspect-[16/6]">
-                                <Image src={item.image} alt={item.title} fill priority className='object-cover' />
+                                <Image src={item.image} alt={item.title}
+                                 fill priority className='object-cover' />
                                 <div className="absolute w-1/3 left-16 md:left-32 top-1/2 transform -translate-y-1/2">
                                     <h2 className={cn('text-xl md:text-6xl font-bold mb-4 text-primary')}>
                                         {item.title}
